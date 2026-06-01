4e0c9673246c4a4fd77e00b474a5bc56f95c533e
liangbingyihao <liangbingyihao@163.com>
2026-06-01 17:23:49 +0800

    auto_rsp

--- app/services/message_service.py
+++ app/services/message_service.py
@@ -109,7 +109,7 @@ class MessageService:
     @staticmethod
     def get_auto_rsp(bundle_id,app_version,lang):
         if bundle_id=="com.graceword.ios":
-            if parse_version(app_version)<(1,0,7):
+            if parse_version(app_version)<(1,0,6):
                 return "[Upgrade required to continue](https://apps.apple.com/us/app/grace-word-bible-ai-journal/id6758707628)"
 
 
